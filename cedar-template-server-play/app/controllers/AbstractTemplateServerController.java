285a801c6c2a4f94e5b2be54538bc98c0df4c2bd
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-03-30 16:16:13 -0700

    Started to implement provenance

--- cedar-template-server-play/app/controllers/AbstractTemplateServerController.java
+++ cedar-template-server-play/app/controllers/AbstractTemplateServerController.java
@@ -1,24 +1,31 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.metadatacenter.constant.ConfigConstants;
 import org.metadatacenter.server.play.AbstractCedarController;
-import org.metadatacenter.server.service.TemplateFieldService;
-import org.metadatacenter.server.service.UserService;
+import org.metadatacenter.server.security.Authorization;
+import org.metadatacenter.server.security.exception.CedarAccessException;
+import org.metadatacenter.server.security.model.IAuthRequest;
+import org.metadatacenter.server.security.model.auth.IAccountInfo;
 import play.Configuration;
 import play.Play;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class AbstractTemplateServerController extends AbstractCedarController {
   protected static List<String> FIELD_NAMES_EXCLUSION_LIST;
   protected static Configuration config;
+  protected static String USER_BASE_PATH;
 
   static {
     config = Play.application().configuration();
     FIELD_NAMES_EXCLUSION_LIST = new ArrayList<>();
     FIELD_NAMES_EXCLUSION_LIST.addAll(config.getStringList(ConfigConstants.FIELD_NAMES_LIST_EXCLUSION));
+    USER_BASE_PATH = config.getString(ConfigConstants.USER_DATA_ID_PATH_BASE);
   }
 
   protected static Integer ensureLimit(Integer limit) {
@@ -58,4 +65,24 @@ public class AbstractTemplateServerController extends AbstractCedarController {
     }
     return null;
   }
+
+
+  protected static void addProvenanceInfo(JsonNode node, IAuthRequest authRequest) {
+    String id = null;
+    try {
+      IAccountInfo accountInfo = Authorization.getAccountInfo(authRequest);
+      id = accountInfo.getId();
+    } catch (CedarAccessException e) {
+      e.printStackTrace();
+    }
+    ObjectNode resource = (ObjectNode) node;
+    Date now = new Date();
+    String nowString = xsdDateTimeFormat.format(now);
+    String userId = USER_BASE_PATH + id;
+    resource.put("pav:createdOn", nowString);
+    resource.put("pav:lastUpdatedOn", nowString);
+    resource.put("pav:createdBy", userId);
+    resource.put("cedar:lastUpdatedBy", userId);
+  }
+
 }
