05c15681fe2f353a1760c35048f3afd18dffa875
johardi <josef.hardi@stanford.edu>
2017-04-05 15:56:46 -0700

    Add request URLs for processing templates

--- cedar-template-server-application/src/test/java/org/metadatacenter/cedar/template/resources/RequestUrls.java
+++ cedar-template-server-application/src/test/java/org/metadatacenter/cedar/template/resources/RequestUrls.java
@@ -10,6 +10,21 @@ public class RequestUrls {
 
   public static final String SERVICE_BASE_URL = "http://localhost";
 
+  public static String forCreatingTemplate(int portNumber, @Nonnull String importMode) {
+    checkNotNull(importMode);
+    return String.format("%s:%d/templates?import_mode=%s", SERVICE_BASE_URL, portNumber, importMode);
+  }
+
+  public static String forDeletingTemplate(int portNumber, @Nonnull String templateId) {
+    checkNotNull(templateId);
+    return String.format("%s:%d/templates/%s", SERVICE_BASE_URL, portNumber, templateId);
+  }
+
+  public static String forFindingTemplate(int portNumber, @Nonnull String templateId) {
+    checkNotNull(templateId);
+    return String.format("%s:%d/templates/%s", SERVICE_BASE_URL, portNumber, templateId);
+  }
+
   public static String forCreatingInstances(int portNumber, @Nonnull String importMode) {
     checkNotNull(importMode);
     return String.format("%s:%d/template-instances?import_mode=%s", SERVICE_BASE_URL, portNumber, importMode);
@@ -36,5 +51,9 @@ public class RequestUrls {
 
   public static String forValidatingField(int portNumber) {
     return String.format("%s:%d/command/validate?resource_types=field", SERVICE_BASE_URL, portNumber);
+  }
+
+  public static String forValidatingInstance(int portNumber) {
+    return String.format("%s:%d/command/validate?resource_types=instance", SERVICE_BASE_URL, portNumber);
   }
 }
