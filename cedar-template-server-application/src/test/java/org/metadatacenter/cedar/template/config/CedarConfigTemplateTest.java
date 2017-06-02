0957d3cced7100eed90f06a00c668c26536e8140
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-02 16:33:07 -0700

    Refactor to facilitate dockerization

--- cedar-template-server-application/src/test/java/org/metadatacenter/cedar/template/config/CedarConfigTemplateTest.java
+++ cedar-template-server-application/src/test/java/org/metadatacenter/cedar/template/config/CedarConfigTemplateTest.java
@@ -23,8 +23,12 @@ public class CedarConfigTemplateTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_KEYCLOAK_CLIENT_ID.getName(), "cedar-angular-app");
 
-    CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_MONGO_USER_NAME, env);
-    CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_MONGO_USER_PASSWORD, env);
+    CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME, env);
+    CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD, env);
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
+
+    env.put(CedarEnvironmentVariable.CEDAR_PORT_TEMPLATE.getName(), "9001");
 
     env.put(CedarEnvironmentVariable.CEDAR_LD_USER_BASE.getName(), "https://metadatacenter.org/users/");
 
