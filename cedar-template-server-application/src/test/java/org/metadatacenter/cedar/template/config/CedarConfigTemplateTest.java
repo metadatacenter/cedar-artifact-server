8f1a7aeb1a3547b17cd5dca01979d3b5091e3111
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-08 15:02:09 -0700

    Refactor environment variables related to Docker

--- cedar-template-server-application/src/test/java/org/metadatacenter/cedar/template/config/CedarConfigTemplateTest.java
+++ cedar-template-server-application/src/test/java/org/metadatacenter/cedar/template/config/CedarConfigTemplateTest.java
@@ -21,16 +21,14 @@ public class CedarConfigTemplateTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_HOST.getName(), "metadatacenter.orgx");
 
-    env.put(CedarEnvironmentVariable.CEDAR_KEYCLOAK_CLIENT_ID.getName(), "cedar-angular-app");
-
     CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME, env);
     CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD, env);
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
 
-    env.put(CedarEnvironmentVariable.CEDAR_PORT_TEMPLATE.getName(), "9001");
-
-    env.put(CedarEnvironmentVariable.CEDAR_LD_USER_BASE.getName(), "https://metadatacenter.org/users/");
+    env.put(CedarEnvironmentVariable.CEDAR_TEMPLATE_HTTP_PORT.getName(), "9001");
+    env.put(CedarEnvironmentVariable.CEDAR_TEMPLATE_ADMIN_PORT.getName(), "9101");
+    env.put(CedarEnvironmentVariable.CEDAR_TEMPLATE_STOP_PORT.getName(), "9201");
 
     CedarEnvironmentUtil.copy(CedarEnvironmentVariable.CEDAR_TEST_USER1_ID, env);
 
