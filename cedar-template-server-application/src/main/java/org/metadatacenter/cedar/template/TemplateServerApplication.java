a8730a693d3537f70c7cbaaf6115f40a87daf855
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-11-20 17:20:31 -0800

    Refactor

--- cedar-template-server-application/src/main/java/org/metadatacenter/cedar/template/TemplateServerApplication.java
+++ cedar-template-server-application/src/main/java/org/metadatacenter/cedar/template/TemplateServerApplication.java
@@ -1,32 +1,17 @@
 package org.metadatacenter.cedar.template;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.mongodb.MongoClient;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.metadatacenter.bridge.CedarDataServices;
 import org.metadatacenter.cedar.template.health.TemplateServerHealthCheck;
 import org.metadatacenter.cedar.template.resources.*;
-import org.metadatacenter.cedar.util.dw.CedarMicroserviceApplication;
+import org.metadatacenter.cedar.util.dw.CedarMicroserviceApplicationWithMongo;
 import org.metadatacenter.config.CedarConfig;
 import org.metadatacenter.config.MongoConfig;
-import org.metadatacenter.model.CedarNodeType;
 import org.metadatacenter.model.ServerName;
-import org.metadatacenter.server.service.TemplateElementService;
-import org.metadatacenter.server.service.TemplateFieldService;
-import org.metadatacenter.server.service.TemplateInstanceService;
-import org.metadatacenter.server.service.TemplateService;
-import org.metadatacenter.server.service.mongodb.TemplateElementServiceMongoDB;
-import org.metadatacenter.server.service.mongodb.TemplateFieldServiceMongoDB;
-import org.metadatacenter.server.service.mongodb.TemplateInstanceServiceMongoDB;
-import org.metadatacenter.server.service.mongodb.TemplateServiceMongoDB;
 
-public class TemplateServerApplication extends CedarMicroserviceApplication<TemplateServerConfiguration> {
-
-  protected static TemplateFieldService<String, JsonNode> templateFieldService;
-  protected static TemplateElementService<String, JsonNode> templateElementService;
-  protected static TemplateService<String, JsonNode> templateService;
-  protected static TemplateInstanceService<String, JsonNode> templateInstanceService;
+public class TemplateServerApplication extends CedarMicroserviceApplicationWithMongo<TemplateServerConfiguration> {
 
   public static void main(String[] args) throws Exception {
     new TemplateServerApplication().run(args);
@@ -43,35 +28,17 @@ public class TemplateServerApplication extends CedarMicroserviceApplication<Temp
 
   @Override
   public void initializeApp() {
-
     MongoConfig templateServerConfig = cedarConfig.getTemplateServerConfig();
     CedarDataServices.initializeMongoClientFactoryForDocuments(templateServerConfig.getMongoConnection());
 
     MongoClient mongoClientForDocuments = CedarDataServices.getMongoClientFactoryForDocuments().getClient();
 
-    templateFieldService = new TemplateFieldServiceMongoDB(
-        mongoClientForDocuments,
-        templateServerConfig.getDatabaseName(),
-        templateServerConfig.getMongoCollectionName(CedarNodeType.FIELD));
-
-    templateElementService = new TemplateElementServiceMongoDB(
-        mongoClientForDocuments,
-        templateServerConfig.getDatabaseName(),
-        templateServerConfig.getMongoCollectionName(CedarNodeType.ELEMENT));
-
-    templateService = new TemplateServiceMongoDB(
-        mongoClientForDocuments,
-        templateServerConfig.getDatabaseName(),
-        templateServerConfig.getMongoCollectionName(CedarNodeType.TEMPLATE));
-
-    templateInstanceService = new TemplateInstanceServiceMongoDB(
-        mongoClientForDocuments,
-        templateServerConfig.getDatabaseName(),
-        templateServerConfig.getMongoCollectionName(CedarNodeType.INSTANCE));
+    initMongoServices(mongoClientForDocuments, templateServerConfig);
   }
 
   @Override
   public void runApp(TemplateServerConfiguration configuration, Environment environment) {
+
     final IndexResource index = new IndexResource();
     environment.jersey().register(index);
 
